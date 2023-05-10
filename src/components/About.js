a688e82c3ccea592faa882536604b140ea417f98
McKenna <Mckennaburch19@gmail.com>
2023-05-09 23:57:11 -0500

    added formatting for layout

--- src/components/About.js
+++ src/components/About.js
@@ -10,13 +10,12 @@ const About = () =>{
             <h2 class="about-header">
                 Welcome to your luxurious home away from home 
             </h2>
-            </div>
-            <div>
-              <img src={pool} alt ={"pool"}/>
-            </div>
             <section>
                 <p>Write a paragraph that talks about your brand or property here. Convince your prospective clients to choose you and your offerings by highlighting the qualities that set you apart from the competition. Your audience is already on your website, so push a little bit harder to seal the deal!</p>
             </section>
+            <img src={pool} alt ={"pool"}/>
+            </div>
+
         </div>
         </section>
         
