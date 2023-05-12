b63fca7e6cdc792a3d70981fdaed5237dff85104
McKenna <Mckennaburch19@gmail.com>
2023-05-11 20:14:20 -0500

    added flex to about section for image

--- src/components/About.js
+++ src/components/About.js
@@ -7,15 +7,18 @@ const About = () =>{
         <section>
         <div class="about-section"> 
             <div class="container">
-            <h2 class="about-header">
+                
+            <div class="left column">
+                <h2 class="about-header">
                 Welcome to your luxurious home away from home 
-            </h2>
-            <section>
+                </h2>
+          
                 <p>Write a paragraph that talks about your brand or property here. Convince your prospective clients to choose you and your offerings by highlighting the qualities that set you apart from the competition. Your audience is already on your website, so push a little bit harder to seal the deal!</p>
-            </section>
-            <img src={pool} alt ={"pool"}/>
+           
             </div>
-
+            <p>test</p>
+            </div>
+            
         </div>
         </section>
         
