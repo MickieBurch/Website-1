0aa38cfda17608e45b83bb687d277a3e62f51342
McKenna <Mckennaburch19@gmail.com>
2023-05-11 20:48:26 -0500

    added the margins to the image for the about section

--- src/components/About.js
+++ src/components/About.js
@@ -4,23 +4,24 @@ import pool from '../images/pool2.webp'
 const About = () =>{
 
     return(
-        <section>
-        <div class="about-section"> 
-            <div class="container">
-                
-            <div class="left column">
-                <h2 class="about-header">
-                Welcome to your luxurious home away from home 
-                </h2>
-          
-                <p>Write a paragraph that talks about your brand or property here. Convince your prospective clients to choose you and your offerings by highlighting the qualities that set you apart from the competition. Your audience is already on your website, so push a little bit harder to seal the deal!</p>
-           
-            </div>
-            <p>test</p>
-            </div>
-            
+<section>
+  <div class="about-section"> 
+    <div class="container">
+      <div class="left-column">
+        <img class="about-image" src={pool} alt="Pool Image"></img>
+        <div class="text-content">
+          <h2 class="about-header">
+            Welcome to your luxurious home away from home 
+          </h2>
+          <p class="about-p"> 
+            Write a paragraph that talks about your brand or property here. Convince your prospective clients to choose you and your offerings by highlighting the qualities that set you apart from the competition. Your audience is already on your website, so push a little bit harder to seal the deal!
+          </p>
         </div>
-        </section>
+      </div>
+    </div>
+  </div>
+</section>
+
         
 
     );
