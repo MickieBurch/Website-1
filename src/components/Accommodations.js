1ad9efa6a2483348cf3221443793c268431f09b1
McKenna <Mckennaburch19@gmail.com>
2023-05-16 12:34:33 -0500

    added in images and formatting for the accommodations section

--- src/components/Accommodations.js
+++ src/components/Accommodations.js
@@ -1,43 +1,48 @@
 import React from "react";
+import hotel1 from '../images/Hotel1.jpg'
+import hotel2 from '../images/hotel2.jpg'
+import hotel3 from '../images/Hotel3.jpg'
 
 const Accommodations = () =>{
 
     return(
         <section>
-        <div class="about-section"> 
+        <div class="accommodations-section"> 
             <div class="container">
-            <h2 class="about-header">
+            <h2 class="accommodations-header">
             Accommodations
             </h2>
             </div>
-            <column>
-            Pictures 1 
-            </column>
-            <column>
-            Pictures 2
-            </column>
-            <column>
-            Pictures 3
-            </column>
 
-            <section>
+            <div class="package-container">
+
+            <section class="Hotel1-col">
+                <img class="Hotel-img" src={hotel1}></img>
+                <div class="Hotel-card">
                 <p>2 adults | 1 child below 7</p>
                 <p>De Luxe Room</p>
                 <p>from $189 a night</p>
+                </div>
             </section>
             
-            <section>
+            <section class="Hotel2-col">
+            <img class="Hotel-img" src={hotel2}></img>
+                <div class="Hotel-card">
                 <p>2 adults | 1 child below 7</p>
                 <p>De Luxe Sea View</p>
                 <p>from $209 a night</p>
+                </div>
             </section>
 
-            <section>
+            <section class="Hotel3-col">
+            <img class="Hotel-img" src={hotel3}></img>
+                <div class="Hotel-card">
                 <p>2 adults | 1 child below 7</p>
                 <p>The Wellhall Family Suite</p>
                 <p>from $399 a night</p>
+                </div>
             </section>
-
+            </div>
 
 
         </div>
