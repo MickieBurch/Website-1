5fb3374d46819ba9461ad66036bb9444de9132cc
McKenna <Mckennaburch19@gmail.com>
2023-05-21 22:16:34 -0500

    added framework to the promos section

--- src/components/Promos.js
+++ src/components/Promos.js
@@ -1,31 +1,42 @@
 import React from "react";
-
+import earlybird from "../images/Front-desk.jpg"
+import WellHall from "../images/logo.jpg"
+import Nightsfree from "../images/Towel.jpg"
 const Promos = () =>{
 
     return(
         <section className="main-container">
-            <div>
-                <div>
+            
+                <div className="accommodations-header">
                     <h1>Promos and Offers</h1>
                 </div>
+                <div className="package-container">
+                    
+                <section class="Hotel1-col">
+                <img class="Hotel-img" src={earlybird}></img>
+                <div class="Hotel-card">
+                <h3>Early Bird Discount</h3>
+                <p>List your offers, promos, or special membership privileges and perks here to entice people to book your property.</p>
+                
+                </div>
+            </section>
 
-                <card>
-                <p>IMG HERE</p>
-                    <h3>Early Bird Discount </h3>
-                    <p>List your offers, promos, or special membership privileges and perks here to entice people to book your property.</p>
-                </card>
-
-                <card>
-                <p>IMG HERE</p>
-                    <h3>Wellhall Members Club</h3>
-                    <p>List your offers, promos, or special membership privileges and perks here to entice people to book your property.</p>
-                </card>
-
-                <card>
-                <p>IMG HERE</p>
-                    <h3>Book 3 Nights, Get 1 Night Free</h3>
-                    <p>List your offers, promos, or special membership privileges and perks here to entice people to book your property.</p>
-                </card>
+            <section class="Hotel1-col">
+                <img class="Hotel-img" src={WellHall}></img>
+                <div class="Hotel-card">
+                <h3>Wellhall Members Club</h3>
+                <p>List your offers, promos, or special membership privileges and perks here to entice people to book your property.</p>
+                
+                </div>
+            </section>
+             
+            <section class="Hotel1-col">
+                <img class="Hotel-img" src={Nightsfree}></img>
+                <div class="Hotel-card">
+                <h3>Book 3 Nights, Get 1 Night Free</h3>
+                <p>List your offers, promos, or special membership privileges and perks here to entice people to book your property.</p>   
+                </div>
+            </section>
             </div>
         </section>
 
